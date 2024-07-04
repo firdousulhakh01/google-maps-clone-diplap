9dfea1c7026f33f6586770bfe1e7c934d1ba80bf
firdousulhakh01 <firdousulhakh@gmail.com>
2024-07-04 20:21:26 +0530

    env file error fixed

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -7,7 +7,7 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
